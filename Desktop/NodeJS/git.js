f9cd520406336adda48bee56a6baa476d44ca9e1
amit <amitec1402@gmail.com>
2025-03-11 18:27:19 -0700

    this is git

---
+++ Desktop/NodeJS/git.js
@@ -0,0 +1,11 @@
+let num1=20
+
+let num2=20
+
+let delcost=30
+let finalcost;
+if(num1+num2 <50){
+    finalcost=num1+num2+delcost
+    console.log("Total cost including delcost is ",finalcost)
+}
+else{console.log("Tot cost will not include shipping cost and finalcost will be ",num1+num2)}
